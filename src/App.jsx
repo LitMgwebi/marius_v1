64c1daf6c79547ca2ba85fbb0a576d21dcee560d
Lithi Mgwebi <lithim11@gmail.com>
2023-07-19 17:12:36 +0200

    Adding Theme Change

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,26 @@ import {
     Route,
 } from "react-router-dom";
 import Items from "./components/Items/Items";
+import useLocalStorage from "use-local-storage";
 
 function App() {
+    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+    const switchTheme = () => {
+        const newTheme = theme === 'light'
+            ? 'dark' : 'light';
+        setTheme(newTheme);
+    }
     return (
-        <div className="App">
-            <Header />
+        <div className="App" data-theme={theme}>
             <BrowserRouter>
+                <Header theme={switchTheme} imgIcon={theme} />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/portfolio" element={<Items /> }/>
                 </Routes>
+                <Footer />
             </BrowserRouter>
-            <Footer />
         </div>
     );
 }
--- src/components/PageEnvelope/Header.jsx
+++ src/components/PageEnvelope/Header.jsx
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom";
+import moon from "../../media/moon.svg";
+import sun from "../../media/sun.svg";
 
-function Header() {
+function Header({ theme, imgIcon }) {
     return (
         <header>
-            <h4>Marius Edel</h4>
+            <div className="siteLinks">
+                <h4>Marius Edel</h4>
+                <div className="headerButton">
+                    {imgIcon === "dark" ? <img src={sun} onClick={theme} className="headerLogo" alt="theme" />
+                        : <img src={moon} onClick={theme} className="headerLogo" alt="theme" />}
+                </div>
+            </div>
             <section className="siteLinks">
                 <a href="#profile">Profile</a>
                 <a href="#portfolio">Portfolio</a>
