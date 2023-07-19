dbddaa2c175ee65fe87cba4e0ee400f74723ea92
Lithi Mgwebi <lithim11@gmail.com>
2023-07-19 15:45:49 +0200

    UpdatingSwipers Items ,Portfolio and index.css. Adding Hubspot capabilities

--- src/components/Home/Portfolio.jsx
+++ src/components/Home/Portfolio.jsx
@@ -1,22 +1,52 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow } from "swiper/modules";
+import { EffectCards, Pagination, Navigation } from "swiper/modules";
 import { Link } from "react-router-dom"
 import "swiper/css";
-import "swiper/css/effect-coverflow";
+import "swiper/css/effect-cards";
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 function Portfolio() {
     return (
         <div id="portfolio">
             <h1>Portfolio</h1>
-            <Swiper
-                modules={[EffectCoverflow]}
-                class="swiper"
-            >
-                <SwiperSlide>This is slide 1</SwiperSlide>
-                <SwiperSlide>This is slide 2</SwiperSlide>
-                <SwiperSlide>This is slide 3</SwiperSlide>
-                <SwiperSlide><Link to="/portfolio">See More</Link></SwiperSlide>
-            </Swiper>
+            <div className="buttonGroup">
+                <button><Link to="/portfolio">View Archive</Link></button>
+            </div>
+            <div className="swiper-slide">
+                <Swiper
+                    effect={"cards"}
+                    pagination={{
+                        type: 'fraction'
+                    }}
+                    navigation={true}
+                    modules={[EffectCards, Pagination, Navigation]}
+                    className="swiper"
+                >
+                    <SwiperSlide className="slide">
+                        <div>
+                            This is slide 1
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className="slide">
+                        <div>
+                            This is slide 2
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className="slide">
+                        <div>
+                            This is slide 3
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className="slide">
+                        <Link to="/portfolio">
+                            <div>
+                                See More
+                            </div>
+                        </Link>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
         </div>
     )
 }
--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -1,7 +1,43 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination } from "swiper/modules";
+import { Link } from "react-router-dom"
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import 'swiper/css/pagination';
 function Items() {
     return (
         <div id="items">
             <h1>Portfolio</h1>
+            <div className="buttonGroup">
+                <button><Link to="/">Back</Link></button>
+            </div>
+            <div className="swiper-slide">
+                <Swiper
+                    effect={"coverflow"}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidePerView={'auto'}
+                    pagination={true}
+                    modules={[EffectCoverflow, Pagination]}
+                    className="itemSwiper"
+                >
+                    <SwiperSlide className="slide">
+                        <div>
+                            This is slide 1
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className="slide">
+                        <div>
+                            This is slide 2
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className="slide">
+                        <div>
+                            This is slide 3
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
         </div>
     )
 }
